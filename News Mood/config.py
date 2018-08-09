fb4eca278bc4a2db909ab990407600e5197cd1a7
Jing X <vera.jxu@gmail.com>
2018-08-08 23:56:19 -0400

    Added solution for social API analytics project

---
+++ News Mood/config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "leIz3VmHZLKFVY9P0y3p0v0C5"
+consumer_secret = "M4GUceSOZF5eHBRkP7iXIvE9fHTfGGlu6zcA8DWjWUgryj3LQ6"
+access_token = "1009238072461062144-vqvQobC5qbaGKv1AQj3S2GisdNMYW1"
+access_token_secret = "C1TnQFXn8aFQKe4JTvwPi11bxsFs1Pap4NnPOF5IRlbIA"
---
+++ PlotBot/config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "mAJLjUYVaeWD4iRXiHR8ckxJt"
+consumer_secret = "uLmcYQqVT7KPm5VRBZksgnhMJF9XVoRTSysMeS5HoeanmVOUIz"
+access_token = "1009238072461062144-rzHTbkUMFMjkzrMDCSNZVPagTF6Yb3"
+access_token_secret = "DdDRnWromEC7FB7rw18X0GVzz6lmgu2ndhVCZb5xwFhsM"
